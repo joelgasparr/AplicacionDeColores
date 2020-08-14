213fca93079b46f1d4d3a0d2edb3513065f36c6e
Joel Moreno <joel32153@hotmail.com>
2020-08-14 01:51:54 -0500

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by Joel Gaspar on 14/08/20.
+//  Copyright © 2020 Joel Gaspar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
